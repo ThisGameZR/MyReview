f7a75f2ca2060869baa2487c142bdfbbb0a73cb1
ThisGameZR <timezazare@gmail.com>
2022-02-28 02:21:56 +0700

    Translattion

--- components/home/Feature.js
+++ components/home/Feature.js
@@ -30,7 +30,7 @@ const features = [
 
 export default function Feature() {
   return (
-    <div className="py-12 bg-white">
+    <div className="py-12 bg-white" id="#feature">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Lorem ipsum, dolor sit amet</h2>
--- components/home/Landing2.js
+++ components/home/Landing2.js
@@ -51,18 +51,18 @@ export default function Landing2() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="#"
+                    href="/auth/register"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Get started
+                    {t("index:landing2_button1")}
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="#pricing"
+                    href="#feature"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
-                    Pricing
+                    {t("index:landing2_button2")}
                   </a>
                 </div>
               </div>
